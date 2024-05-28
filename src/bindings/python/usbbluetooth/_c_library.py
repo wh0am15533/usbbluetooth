a8150110b45a16d3b927bc4ef830289eefd93f26
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2024-05-28 13:48:01 +0200

    bindings: python: Initial implementation

---
+++ src/bindings/python/usbbluetooth/_c_library.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+import sys
+import ctypes
+from enum import IntEnum
+from importlib.resources import files
+
+
+def _lib_name() -> str:
+    '''
+    Returns an string with the expected library name for the current platform.
+    '''
+    lib_names = {
+        'darwin': 'libusbbluetooth.dylib',
+        'win32': 'usbbluetooth.dll',
+        'linux': 'libusbbluetooth.so'
+    }
+    return lib_names.get(sys.platform, 'libusbbluetooth.so')
+
+
+def _load_lib():
+    '''
+    Attempt to load a library from package resources.
+    '''
+    library_path = files('usbbluetooth.lib').joinpath(_lib_name())
+    try:
+        return ctypes.cdll.LoadLibrary(str(library_path))
+    except OSError as e:
+        print(e)
+        return None
+
+
+# Attempt to load the corresponding C library...
+_lib = _load_lib()
+if _lib is None:
+    raise ImportError(
+        "ERROR: Failed to load the dynamic library for usbbluetooth.")
+
+
+class _CStatus(IntEnum):
+    OK = 0
+    ERR_UNK = -1,
+    ERR_NOMEM = -2,
+    ERR_DEVICE_CLOSED = -3,
+
+
+class _CDevice(ctypes.Structure):
+    _fields_ = (("ref_count", ctypes.c_uint8),
+                ("device", ctypes.c_voidp),
+                ("vendor_id", ctypes.c_uint16),
+                ("product_id", ctypes.c_uint16),
+                ("handle", ctypes.c_voidp),
+                ("interface_num", ctypes.c_uint8),
+                ("epnum_evt", ctypes.c_uint8),
+                ("epnum_acl_in", ctypes.c_uint8),
+                ("epnum_acl_out", ctypes.c_uint8))
+
+
+def _setup_func(lib, fname, restype, argtypes) -> None:
+    '''
+    Setup function type info. Export as global.
+    '''
+    try:
+        method = getattr(lib, fname)
+        method.restype = restype
+        method.argtypes = argtypes
+        globals()[fname] = method
+    except AttributeError:
+        raise ImportError(
+            f"ERROR: Fail to setup '{fname}' function prototype. Make sure you are loading the correct library version.")
+
+
+# Actual function prototypes...
+_setup_func(_lib, "usbbluetooth_init", _CStatus, None)
+_setup_func(_lib, "usbbluetooth_exit", None, None)
+_setup_func(_lib, "usbbluetooth_get_device_list", _CStatus,
+            [ctypes.POINTER(ctypes.POINTER(ctypes.POINTER(_CDevice)))])
+_setup_func(_lib, "usbbluetooth_free_device_list", None,
+            [ctypes.POINTER(ctypes.POINTER(ctypes.POINTER(_CDevice)))])
+_setup_func(_lib, "usbbluetooth_reference_device", ctypes.POINTER(_CDevice),
+            [ctypes.POINTER(_CDevice)])
+_setup_func(_lib, "usbbluetooth_unreference_device", None,
+            [ctypes.POINTER(ctypes.POINTER(_CDevice))])
+_setup_func(_lib, "usbbluetooth_open", _CStatus,
+            [ctypes.POINTER(_CDevice)])
+_setup_func(_lib, "usbbluetooth_close", None,
+            [ctypes.POINTER(_CDevice)])
+_setup_func(_lib, "usbbluetooth_write", _CStatus,
+            [ctypes.POINTER(_CDevice), ctypes.POINTER(ctypes.c_uint8), ctypes.c_uint16])
+_setup_func(_lib, "usbbluetooth_read", _CStatus,
+            [ctypes.POINTER(_CDevice), ctypes.POINTER(ctypes.c_uint8), ctypes.POINTER(ctypes.c_uint16)])
---
+++ src/bindings/python/usbbluetooth/core.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+import ctypes
+import _c_library
+
+
+def _check_status(status: _c_library._CStatus):
+    '''
+    Check an status and raise error if needed
+    '''
+    if status == _c_library._CStatus.OK:
+        return
+    elif status == _c_library._CStatus.ERR_UNK:
+        raise Exception("Unknown error!")
+    elif status == _c_library._CStatus.ERR_NOMEM:
+        raise Exception("Out of memory error!")
+    else:
+        raise Exception("Undocumented error!")
+
+
+class _UsbBluetoothContext():
+    '''
+    Handle USB context creation and destruction.
+    '''
+
+    def __init__(self):
+        _check_status(_c_library.usbbluetooth_init())
+
+    def __del__(self):
+        _c_library.usbbluetooth_exit()
+
+
+class UsbBluetoothDevice():
+    '''
+    USB Bluetooth device.
+    '''
+
+    def __init__(self, ctx: _UsbBluetoothContext, dev: ctypes.POINTER(_c_library._CDevice)):
+        self._ctx = ctx
+        self._dev = _c_library.usbbluetooth_reference_device(dev)
+
+    def __del__(self):
+        _c_library.usbbluetooth_unreference_device(ctypes.byref(self._dev))
+
+    def open(self):
+        _check_status(_c_library.usbbluetooth_open(self._dev))
+
+    def close(self):
+        _c_library.usbbluetooth_close(self._dev)
+
+    def __enter__(self):
+        self.open()
+        return self
+
+    def __exit__(self, type, value, tb):
+        self.close()
+
+    def write(self, data: bytearray):
+        _check_status(_c_library.usbbluetooth_write(
+            self._dev, ctypes.cast(ctypes.c_char_p(data), ctypes.POINTER(ctypes.c_uint8)), len(data)))
+
+    def read(self, bufsize=64):
+        data = (ctypes.c_uint8 * bufsize)()
+        datalen = ctypes.c_uint16(bufsize)
+        _check_status(_c_library.usbbluetooth_read(self._dev, data, datalen))
+        return bytearray(data[0:datalen.value])
+
+
+def list_devices() -> list[UsbBluetoothDevice]:
+    '''
+    List all available Bluetooth devices.
+    '''
+    ctx = _UsbBluetoothContext()
+    c_list = ctypes.POINTER(ctypes.POINTER(_c_library._CDevice))()
+    dev_list = []
+    _check_status(_c_library.usbbluetooth_get_device_list(
+        ctypes.byref(c_list)))
+    i = 0
+    while True:
+        dev_ref = c_list[i]
+        i += 1
+        if not dev_ref:
+            break
+        dev_list.append(UsbBluetoothDevice(ctx, dev_ref))
+    _c_library.usbbluetooth_free_device_list(ctypes.byref(c_list))
+    return dev_list
